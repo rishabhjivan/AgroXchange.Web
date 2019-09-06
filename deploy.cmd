4ad143e0c981955bd32b2d00810237dca1c300ba
Rishabh Jivan <rishabhjivan@gmail.com>
2019-09-06 15:05:58 +0530

    Changed dist path

--- deploy.cmd
+++ deploy.cmd
@@ -101,13 +101,13 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 
 :: 3. Angular Prod Build //If you had generated this yourself then please add this step manually!!)
 IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
-echo Building App in %DEPLOYMENT_SOURCE%…
-pushd "%DEPLOYMENT_SOURCE%"
-:: call :ExecuteCmd !NPM_CMD! run build
-:: If the above command fails comment above and uncomment below one
-call ./node_modules/.bin/ng build –prod
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
+  echo Building App in %DEPLOYMENT_SOURCE%…
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd !NPM_CMD! run build
+  :: If the above command fails comment above and uncomment below one
+  :: call ./node_modules/.bin/ng build –prod
+  :: IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
 
 :: 4. KuduSync
